968cc7cdbd7f34e3141061c1b6649775c7bce4f8
Douglas Barreto Zanata <44613455+ttiozinho00@users.noreply.github.com>
2026-07-24 08:54:52 -0400

    Update palavras.c
    atualizado

--- src/palavras.c
+++ src/palavras.c
@@ -5,15 +5,23 @@
 #include <time.h>
 #include "../include/palavras.h"
 
+#define ARQUIVO_PALAVRAS "assets/palavras.txt"
+
 int sortearPalavra(Palavra *p) {
-    FILE *arquivo = fopen("palavras.txt", "r");
-    if (arquivo == NULL) return 0;
+    FILE *arquivo = fopen(ARQUIVO_PALAVRAS, "r");
+    if (arquivo == NULL)
+    {
+         return 0;
+    }
 
     int totalPalavras = 0;
     char linha[200];
 
     while (fgets(linha, sizeof(linha), arquivo) != NULL) {
-        if (strlen(linha) > 2) totalPalavras++;
+        if (strlen(linha) > 2) 
+        {
+            totalPalavras++;
+        }
     }
 
     if (totalPalavras == 0) {
@@ -33,13 +41,21 @@ int sortearPalavra(Palavra *p) {
     linha[strcspn(linha, "\n")] = 0;
 
     char *token = strtok(linha, ";");
-    if (token != NULL) strcpy(p->palavra, token);
+    if (token != NULL) 
+    {
+        strcpy(p->palavra, token);
+    }
     
     token = strtok(NULL, ";");
-    if (token != NULL) strcpy(p->categoria, token);
+    if (token != NULL) 
+    {
+        strcpy(p->categoria, token);
+    }
     
     token = strtok(NULL, ";");
-    if (token != NULL) strcpy(p->dica, token);
-
+    if (token != NULL) 
+    {
+        strcpy(p->dica, token);
+    }
     return 1;
 }
